11e96f9e968d2afdd8c74eadcab35304ef41d157
Victor Valencia <vvalencia@salutarehealth.com>
2020-06-08 02:13:29 -0500

    legal documents meta-boxes

---
+++ parts/meta-boxes/project-legal-info.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Title: Legal Information
+Post Type: res_project
+Order: 4
+*/
+
+piklist('field', array(
+    'type'  => 'editor',
+    'field' => 'restate_project_legal_info',
+    'label' => __('Legal information', 'restate-api')
+));
---
+++ parts/meta-boxes/project-legal.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Title: Legal Documents
+Post Type: res_project
+Order: 5
+*/
+
+$name = array(
+    'type'      => 'text',
+    'field'     => 'restate_project_legal_doc_name',
+    'columns'   => 12,
+    'label'     => esc_html__('Name','restate-api')
+);
+
+$file = array(
+    'type'  => 'file',
+    'field'     => 'restate_project_legal_doc_file',
+    'label'     => esc_html__('Document','restate-api'),
+    'options'       => ['button' => esc_html__('Legal Document', 'restate-api')],
+    'columns'   => 12,
+    'validate'  => array(
+        array(
+            'type' => 'limit',
+            'options' => array(
+                'min' => 0,
+                'max' => 1
+            )
+        )
+    )
+);
+
+piklist('field', array(
+    'type'      => 'group',
+    'field'     => 'restate_project_legal_docs',
+    'template'  => 'field',
+    'add_more'  => true,
+    'fields'    => array(
+        $name,
+        $file,
+    )
+));
